8dc7ac3d41928a1be538f86575dbbec887b08766
fiokee <smleffiong@gmail.com>
2024-02-18 20:59:18 -0800

    handling errors

--- app.js
+++ app.js
@@ -7,5 +7,14 @@ const app = express();
 
 app.use('/api/places', placesRoute);
 
+//error handling
+app.use((error, req, res, next)=>{
+    if(res.headerSent){
+       next(error);
+    }
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occured!'});
+});
+
 
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -21,16 +21,30 @@ router.get('/:pid', (req, res, next)=>{
     const placeId = req.params.pid;
     const place = DUMMY_PLACES.find(p =>{
         return p.id === placeId
-    })
+    });
+
+    //handling places error
+    if(!place){
+       const error = new Error('Could not find a place for the provided id!');
+       error.code = 404;
+       throw error;
+    }
+
     res.json({place: place})
 });
 
 router.get('/user/:uid', (req, res, next)=>{
     const userId = req.params.uid;
-    const place = DUMMY_PLACES.find(p =>{
+    const place = DUMMY_PLACES.find(p=>{
         return p.creator === userId;
     });
-    res.json({place});
+    
+//handling error
+    if(!place){
+        const error = new Error('could not find a place for user with provided id!');
+        error.code = 404;
+        return next(error);
+    }
+    res.json({place})
 })
-
 module.exports = router;
