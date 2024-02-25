ccce81ad6955f2b2b57400a12d4b84c0ecc2b955
fiokee <smleffiong@gmail.com>
2024-02-25 11:40:37 -0800

    setting up users route and middleware

---
+++ routes/users-routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const usersControllers = require('/');
+
+const router = express.Router();
+
+router.get('/:uid',);
+
+router.post('/signup',);
+
+router.post('/login');
+
+module.exports = router;
