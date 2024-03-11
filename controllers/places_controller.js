9310f53c2bdef1f82163495abca3c12d11488a3c
fiokee <smleffiong@gmail.com>
2024-03-10 21:55:03 -0700

    getting placebyid from database

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -20,20 +20,25 @@ let DUMMY_PLACES = [
 ];
 
 
-const getPlaceById = (req, res, next)=>{
+const getPlaceById = async (req, res, next)=>{
 
     const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p =>{
-        return p.id === placeId
-    });
+    let place;
+
+    try{
+
+        place = await Place.findById(placeId);
+    }catch(err){
+        const error = new HttpError('Something went wrong,could not find a place', 500);
+        return next(error);
+    }
 
     //handling places error
     if(!place){
-
-       throw new HttpError ('Could not find a place for the provided id!', 404);
+       return next( new HttpError ('Could not find a place for the provided id!', 404));
     }
 
-    res.json({place: place})
+    res.json({place: place.toObject( {getters: true}) })
 };
 
 const getPlacesByUserId = (req, res, next)=>{
@@ -51,11 +56,12 @@ const getPlacesByUserId = (req, res, next)=>{
 };
 
 //creating  place route or post
-const createPlace = (req, res, next)=>{
+const createPlace = async (req, res, next)=>{
  const errors =  validationResult(req);
  
  if(!errors.isEmpty()){ //checking to validate if the input field is  empty
-    throw new HttpError('Invalid input passed, please check your data', 422);
+    console.log(errors.array());
+    return next(new HttpError('Invalid input passed, please check your data', 422));
  }
 const {title, description, coordinates, address, creator} = req.body;
 //const title = req.body
@@ -67,13 +73,17 @@ const createdPlace = new Place({
     address,
     creator
 });
+try{
 
-createdPlace.save();
+    await createdPlace.save();
+}catch(err){
+    const error = new HttpError('creating place failed try again', 500);
+    return next(error);
+}
 
 res.status(201).json({place: createdPlace});
 // console.log(createdPlace);
 }
-
 
 //update a place by user id
 const updatePlace = ((req, res, next)=>{
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -11,17 +11,23 @@ router.get('/:pid', placesControllers.getPlaceById );
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
+// validating and emtyp input field
 router.post('/',
 [ 
     check('title')
+    .trim()
     .not()
-    .isEmpty(),
-    check('description').not().isLength({min: 5}),
-    check('address').not().isEmpty()
+    .isEmpty()
+    .withMessage('must not be empty'),
+    check('description')
+    .trim()
+    .isLength({min: 10})
+    .withMessage('Description must be at least 10 characters long'),
+    check('address').trim().not().isEmpty()
 
 ], placesControllers.createPlace);
 
-
+// validating and emtyp input field
 router.patch('/:pid',
 [
 check('title')
