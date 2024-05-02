d1a6c4e3e663deabfe0ca14e163fcd11e9ac944c
fiokee <smleffiong@gmail.com>
2024-05-01 21:21:21 -0700

    working on token

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,6 +1,8 @@
     const {v4: uuidv4}= require('uuid');
 
     const User = require('../models/user');
+    const bcrypt = require('bcryptjs'); //hasing password
+    const jwt = require('jsonwebtoken');
 
     const {validationResult} = require('express-validator');
     const HttpError = require('../models/http_error');
@@ -11,7 +13,7 @@
         let users;
         try{
 
-            users = await User.find({}, '-password'); //exempting the password
+            users = await User.find({}, '-password'); //exempting the password when getting users
         }catch(err){
             const error = new HttpError('failed getting users, please try again', 500);
             return next(error);
@@ -41,12 +43,22 @@
     if(existingUser){
         const error = new HttpError('user exist already login instead', 422);
         return next(error)
+    };
+// hashing the user password
+    let hashedPassword;
+    try {
+       hashedPassword = await bcrypt.hash(password, 12);
+    } catch (err) {
+        const error = new HttpError('could not create a user please try again', 500);
+        return next(error)
     }
+    
+
     const createdUser = new User({
         name,
         email,
         image: req.file.path,
-        password,
+        password: hashedPassword,
         places: []
     });
     try{
@@ -54,8 +66,20 @@
     }catch(err){
         const error = new HttpError('Signing Up user failed, please try again', 500);
         return next(error);
+    };
+//generating webtoken jwt for users
+    let token;
+    try {
+        token = jwt.sign({userId: createdUser.id, email: createdUser.email}, 
+            'supersecret_dont_share_2024',
+             {expiresIn: '1h'}
+            );
+    } catch (err) {
+        const error = new HttpError('Signing Up user failed, please try again', 500);
+        return next(error);
     }
-    res.status(201).json({user: createdUser.toObject({getters: true})});
+    
+    res.status(201).json({userId: createdUser.id, email: createdUser.email, token: token }); //data to send back to cleint or frontend
     };
 
     const login = async (req, res, next)=>{
@@ -67,15 +91,45 @@
             existingUser = await User.findOne({email: email})
 
         }catch(err){
-            const error = new HttpError('login up failed please try again', 500);
+            const error = new HttpError('login failed please try again', 500);
             return next(error);
         }
-    //checking to see whether the eamil or password exist in db
-    if(!existingUser || existingUser.password !== password){
+    //checking to see whether the email or password exist in db
+    if(!existingUser){
     const error = new HttpError('invalid credentials, could not login user', 401);
     return next(error);
     }
-        res.json({message: 'Login successful', user: existingUser.toObject({getters: true})});
+
+    let isValidPassword = false;
+    try {
+        
+        isValidPassword = await bcrypt.compare(password, existingUser.password) //comparing plan pass with the hash pass in db
+    } catch (err) {
+        const error = new HttpError(
+            'could not login you, please check credentials and try again',
+            500
+        );
+        return next(error);
+    }
+
+    if(!isValidPassword){
+        const error = new HttpError('invalid credentials, could not login user', 401);
+        return next(error);
+    }
+
+    //generating webtoken jwt for users
+    let token;
+    try {
+        token = jwt.sign({userId: existingUser.id, email: existingUser.email}, 
+            'supersecret_dont_share_2024',
+             {expiresIn: '1h'}
+            );
+    } catch (err) {
+        const error = new HttpError('login user failed, please try again', 500);
+        return next(error);
+    }
+
+        res.json({userId: existingUser.id, email: existingUser.email, token: token});
     };
 
     exports.getUsers = getUsers;
